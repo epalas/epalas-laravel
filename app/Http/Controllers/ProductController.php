5f47db345c6072b2cc836b4d53c92585cc1bfefb
Pedro Garcia <phgarcia93@gmail.com>
2019-06-03 16:07:25 +0200

    Product Controller update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,19 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contvin;
+use App\Vin;
+use App\Prixprod;
 
 class ProductController extends Controller
 {
     public function index() {
         // select all from vin
-        $vins = \App\Vin::all();
+        $vins = Vin::all();
         // select all from prixprod
-        $prix = \App\Prixprod::all();
+        $prix = Prixprod::all();
         // id du vin à récupérer depuis la vue filtres
         $id = 4;
         // select contenant from vin where id
-        $contVin = \App\Contvin::all()
-            ->where('id', 2);
+        $contVinId = Contvin::select('idCont')->where('idVin', $id);
+
+        dd($contVinId);
+//        Table::select('name','surname')->where('id', 1)->get();
 
         return view('produit', compact('vins'))->with(
             [

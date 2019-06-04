3b5dc57d7fc6f93f3c4e2105bc5cdc6f4c48e552
Steve Maibach <steve.maibach@heig-vd.ch>
2019-06-04 12:55:57 +0200

    Correction erreur redirection login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/customer-account';
+    protected $redirectTo = '/customer';
 
     /**
      * Create a new controller instance.

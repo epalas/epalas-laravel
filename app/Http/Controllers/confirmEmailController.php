6451f8045b11be4b465737db2736a9366396e40c
Pedro Garcia <phgarcia93@gmail.com>
2019-06-07 17:01:51 +0200

    fix error email

--- app/Http/Controllers/confirmEmailController.php
+++ app/Http/Controllers/confirmEmailController.php
@@ -22,15 +22,8 @@ class confirmEmailController extends Controller
     $datas['cart'] = Cart::content();
     $datas['total'] = Cart::total();
 
-<<<<<<< HEAD
-    return view('viewEmail')->with('datas', $datas);
-
     Mail::send('viewEmail', $datas, function($message) {
-        $message->to('epalas.agency@gmail.com')->subject('Confirmation de commande')->from('info@gazzar.ch','Gazzar Vins');    
-=======
-    Mail::send('viewEmail', $datas, function($message) {
-        $message->to(Auth::user()->email)->subject('Confirmation de commande')->from('info@gazzar.ch','Gazzar Vins');    
->>>>>>> 818e4d6e81b9af0b15ca032195c01df3a67dd67d
+        $message->to(Auth::user()->email)->subject('Confirmation de commande')->from('info@gazzar.ch','Gazzar Vins');
     });
 
     Cart::destroy();

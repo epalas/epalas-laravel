b8568356db4570170cd077bc64cd41ae4f984a0e
Steve Maibach <steve.maibach@heig-vd.ch>
2019-06-12 15:29:33 +0200

    Page contact avec message

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,7 +7,7 @@ Gazzar - Contact
 @section('contenu')
 <div class="container mt-5">
 
-  <h1>Contacter nous</h1>  
+  <h1>Contactez nous</h1>  
   <p>Nous nous tenons volontiers à votre disposition pour répondre à vos questions. <br>
   Merci de remplir le formulaire ci-dessous pour nous transmettre votre demande.</p>        
 
@@ -30,28 +30,33 @@ Gazzar - Contact
     </div>
     <div class="row mt-3">
       <div class="col-md-3">
-        <input name="nom" type="text" class="form-control" placeholder="Nom">
+        <input name="nom" type="text" class="form-control" placeholder="Nom" required>
       </div>
       <div class="col-md-3">
-        <input name="prenom" type="text" class="form-control" placeholder="Prénom">
+        <input name="prenom" type="text" class="form-control" placeholder="Prénom" required>
       </div>
     </div>
     <div class="row mt-3">
       <div class="col-md-3">
-        <input name="telephone" type="text" class="form-control" placeholder="Téléphone">
+        <input name="telephone" type="text" class="form-control" placeholder="Téléphone" required>
       </div>
       <div class="col-md-3">
-        <input name="mail" type="email" class="form-control" placeholder="E-mail">
+        <input name="mail" type="email" class="form-control" placeholder="E-mail" required>
       </div>
     </div>
     <div class="row mt-3">
       <div class="col-md-6">
         <div class="form-group">
-          <textarea name="message" class="form-control" id="message" rows="8" placeholder="Votre message..."></textarea>
+          <textarea name="message" class="form-control" id="message" rows="8" placeholder="Votre message..." required></textarea>
         </div>
       </div>
     </div>
-    <button type="submit" class="btn btn-primary mr-auto">Envoyer</button>
+    <button id="send" type="submit" class="btn btn-primary mr-auto">Envoyer</button>
+  
+        <div id="advertise" class="bg-secondary text-white rounded col-auto mt-3 pt-3 pb-1 text-center hidden">
+          <p>Merci pour votre demande. Nous allons la traiter dans les plus bref délais.</p>
+        </div>
+    
   </form>
 </div> 
 @endsection
